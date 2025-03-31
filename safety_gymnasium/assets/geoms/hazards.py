1333c685ab85342384f396064a4cd6f14d4a9e7b
Hafizur <hafiz.buetipe@gmail.com>
2025-03-31 17:31:14 -0400

    update assets

--- safety_gymnasium/assets/geoms/hazards.py
+++ safety_gymnasium/assets/geoms/hazards.py
@@ -36,8 +36,9 @@ class Hazards(Geom):  # pylint: disable=too-many-instance-attributes
     alpha: float = COLOR['hazard'][-1]
     cost: float = 1.0  # Cost (per step) for violating the constraint
 
-    color: np.array = COLOR['hazard']
-    group: np.array = GROUP['hazard']
+    color: np.array = field(default_factory=lambda: np.array(COLOR['hazard']))
+    group: np.array = field(default_factory=lambda: np.array(GROUP['hazard']))
+
     is_lidar_observed: bool = True
     is_constrained: bool = True
     is_meshed: bool = False
